176eca8591ea37988fb3bf9cea2b350e601c306c
Alvis <q8956895@gmail.com>
2023-01-07 12:02:04 +0800

    4th version

--- gitSample/src/gitSample/gitext.java
+++ gitSample/src/gitSample/gitext.java
@@ -4,5 +4,6 @@ public class gitext {
 	public static void main(String[] args) {
 		System.out.println("2nd version");
 		System.out.println("3nd version");
+		System.out.println("4nd version");
 	}
 }
